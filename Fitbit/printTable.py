680312f1020069e3a0ecdb763d0316aa011be004
AndiH <a.herten+github@gmail.com>
2013-11-17 16:03:48 +0100

    Changed print to desc ordering; converted date to readable output while printing table contents

--- Fitbit/printTable.py
+++ Fitbit/printTable.py
@@ -1,5 +1,6 @@
 import sqlite3 as sql
 import argparse
+import time
 
 parser = argparse.ArgumentParser(description='Prints * from table of SQLite3 db file')
 parser.add_argument('db', metavar='DBFILE', type=str, help='Name of the SQLite3 db file')
@@ -9,7 +10,7 @@ args = parser.parse_args()
 dbConnection = sql.connect(args.db)
 with dbConnection:
 	dbCursor = dbConnection.cursor()
-	dbCursor.execute("select * from " + args.t)
+	dbCursor.execute("select * from " + args.t + " order by date DESC")
 	rows = dbCursor.fetchall()
-	for row in rows:
-		print row
+for row in rows:
+	print time.strftime('%d-%m-%Y', time.localtime(row[0])), row
