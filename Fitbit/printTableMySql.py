2824bdbe15e463ae8d468f2015b98a9e4b489896
AndiH <a.herten+github@gmail.com>
2014-01-09 19:20:21 +0100

    Cosmetic changes.

--- Fitbit/printTableMySql.py
+++ Fitbit/printTableMySql.py
@@ -2,18 +2,18 @@
 # Usage: printTableMySql.py Fitbit
 import argparse
 import time
-import sqlInfo
 import oursql
+import sqlInfo
 
-parser = argparse.ArgumentParser(description='Prints * from a table of a MySql db')
-parser.add_argument('t', metavar='TABLE', type=str, help='Name of the table to be printed')
+parser = argparse.ArgumentParser(description='Prints * from a table of a MySQL db')
+parser.add_argument('-t', '--table', default='Fitbit', help='Name of SQL table name')
 args = parser.parse_args()
-tableName = args.t
+tableName = args.table
 
 db_connection = oursql.connect(host=sqlInfo.host, user=sqlInfo.user, passwd=sqlInfo.password, db=sqlInfo.database)
 cur = db_connection.cursor()
 
-cur.execute("select * from " + tableName + " order by date DESC")
+cur.execute("select * from " + tableName + " order by date ASC")
 rows = cur.fetchall()
 for row in rows:
 	print time.strftime('%d-%m-%Y', time.localtime(row[0])), row
