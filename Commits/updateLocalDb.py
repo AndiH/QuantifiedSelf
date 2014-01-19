6233a24c77e4f672a37973cd04272a214bf8ec66
André Goerres <andre.goerres+git@gmail.com>
2014-01-19 14:02:47 +0100

    Updated the update DB script, call from a bash script not needed anymore.

--- Commits/updateLocalDb.py
+++ Commits/updateLocalDb.py
@@ -1,28 +1,104 @@
 #!/usr/bin/env python
+from socket import gethostname
 import sqlite3 as sql
-import argparse
-import datetime
+import os, sys
+import git
 
-def updateDb(args):
-	date = args.date
-	hashValue = args.hash
-	commitMessage = args.message
 
-	if (-1 == date):
-		date = datetime.datetime.now()
-	else:
-		date = datetime.datetime.fromtimestamp(date)
+# The path to the local database, please adjust to your setting
+def getPathToDB():
+  return '/Users/andre/Development/QuantifiedSelf/Commits/gitCommits.db'
 
-	print date, hashValue, commitMessage
-	dbConnection = sql.connect('/Users/Andi/Documents/Coding/QuantifiedSelf/Commits/gitCommits.db')
-	with dbConnection:
-		dbCursor = dbConnection.cursor()
-		dbCursor.execute("INSERT INTO commits VALUES (?,?,?)", (hashValue, date.strftime("%s"),commitMessage))
+
+def getMachineName():
+  return gethostname()
+
+
+# Extract the repository information. If numCommits is set > 0, it will extract
+# only this amount of commits.
+def extractRepoInfo(repo, numCommits = 0):
+  commitInfo = []
+  commits = list(repo.iter_commits())
+
+  userConf = repo.config_reader('global')
+  userName = userConf.get('user', 'name')
+  userEmail = userConf.get('user', 'email')
+  if userEmail.startswith('"') and userEmail.endswith('"'):
+    userEmail = userEmail[1:-1]
+
+  for commit in commits:
+    # check if author of commit is the same as the local user
+    author = commit.author.name.encode('utf8')
+    email = commit.author.email.encode('utf8')
+    if author != userName or email != userEmail:
+      continue
+
+    commitInfo.append({
+      'hash': commit.name_rev.split(' ')[0],
+      'message': commit.message.encode('utf8').strip(),
+      'timestamp': commit.authored_date,
+      'path': repo.git_dir[:-4],
+      'filesAdded': [],   # todo
+      'filesChanged': [], # todo
+      'filesRemoved': [], # todo
+      'linesAdded': 0,    # todo
+      'linesRemoved': 0,  # todo
+      'machine': getMachineName()
+    })
+
+    if numCommits > 0 and len(commitInfo) >= numCommits:
+      break
+  return commitInfo
+
+
+def fillDatabase(repoInfo):
+  con = None
+  try:
+    con = sql.connect(getPathToDB())
+    cur = con.cursor()
+    for commit in repoInfo:
+      cur.execute(
+        "INSERT OR REPLACE INTO commits VALUES (?,?,?,?,?,?,?,?,?,?)",
+        (
+          commit['hash'],
+          commit['timestamp'],
+          commit['message'],
+          ','.join(commit['filesAdded']),
+          ','.join(commit['filesChanged']),
+          ','.join(commit['filesRemoved']),
+          commit['linesAdded'],
+          commit['linesRemoved'],
+          commit['path'],
+          commit['machine']
+        )
+      )
+    con.commit()
+
+  except sql.Error, e:
+    print "Error %s:" % e.args[0]
+    sys.exit(1)
+
+  finally:
+    if con:
+      con.close()
+
+
+def pathContainsGitRepo(path):
+  for root, dirs, files in os.walk(path):
+    if '.git' in dirs:
+      return True
+    else:
+      return False
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Update local SQLite3 DB with commit')
-	parser.add_argument('hash', metavar='HASH', type=str, help='Hash value of commit')
-	parser.add_argument('-d', '--date', default=-1, type=int, help='Date of commit')
-	parser.add_argument('-msg', '--message', default='Lorem', help='Commit message')
-	args = parser.parse_args()
-	updateDb(args)
+  repoPath = os.getcwd()
+  if not pathContainsGitRepo(repoPath):
+    print "No git repository in the current working directory. Please call this script from within a git repo."
+    sys.exit(0)
+
+  repoInfo = extractRepoInfo(git.Repo(repoPath), 1)
+  fillDatabase(repoInfo)
+
+  print "Inserted commit {} into database.".format(repoInfo[0]['hash'][:10])
+
