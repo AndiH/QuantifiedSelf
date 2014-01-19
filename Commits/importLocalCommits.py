d57c018f459c6aa29ba25c29c02b795e7d64d567
André Goerres <andre.goerres+git@gmail.com>
2014-01-19 16:26:15 +0100

    Increase usability of the scripts.

--- Commits/importLocalCommits.py
+++ Commits/importLocalCommits.py
@@ -2,29 +2,41 @@
 import os
 import git
 import updateLocalDb
-
-# configuration values
-_pathToDB = '/Users/andre/Development/QuantifiedSelf/Commits/gitCommits.db'
+import argparse
 
 
-def getLocalGitRepos():
+def getLocalGitRepos(args):
   listOfRepos = []
-  for root, dirs, files in os.walk(os.path.expanduser('~/')):
-    if '.git' in dirs:
-      listOfRepos.append(root)
+  for path in args.paths:
+    # do some normilazation on the path first
+    path = os.path.abspath(os.path.expanduser(path))
 
-    # during development we don't want to wait too long
-    if len(listOfRepos) > 3:
-      break
+    for root, dirs, files in os.walk(os.path.expanduser(path)):
+      if '.git' in dirs and root not in listOfRepos:
+        listOfRepos.append(root)
+
+      # skip going further down the rabbit hole
+      if args.no_recursive:
+        break
+
+      # during development we don't want to wait too long
+      if args.max_repos > 0 and len(listOfRepos) >= args.max_repos:
+        break
+
   return listOfRepos
 
 
 if __name__ == "__main__":
-  commitCounter = 0
+  parser = argparse.ArgumentParser(description='Search for existing Git repositories and import commits made by the local user.')
+  parser.add_argument('--max-repos', metavar='N', type=int, default=0, help="stop scanning after N repositories")
+  parser.add_argument('--no-recursive', action='store_true', help="don't scan recursively")
+  parser.add_argument('--paths', nargs='+', type=str, default=["~/"], help="Paths to recursively scan for Git repositories. If not specified, the user's home directory (~/) is scanned.")
+  args = parser.parse_args()
 
   # get all local repositories (this might take a while)
-  localGitRepos = getLocalGitRepos()
+  localGitRepos = getLocalGitRepos(args)
 
+  commitCounter = 0
   for repoPath in localGitRepos:
     # get the relevant infos from the repository
     repoInfo = updateLocalDb.extractRepoInfo(git.Repo(repoPath))
