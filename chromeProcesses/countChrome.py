d4b628788b08d63acc33f14670571529dca02226
AndiH <a.herten+github@gmail.com>
2014-01-24 18:37:37 +0100

    Counting Chrome tabs now runs completely in Python and SQLite and also saves the proper amount of tabs.

--- chromeProcesses/countChrome.py
+++ chromeProcesses/countChrome.py
@@ -6,7 +6,7 @@ import sys
 def getPathToDB():
   return '/Users/Andi/Documents/Coding/QuantifiedSelf/chromeProcesses/chromeProcesses.db'
 
-def getChromeProcesses():
+def getChromeProcessesOld(): # The way it was done before... subtracting a fixed threshold is wrong, though
 	chromeOffSet = 13
 	nOfChromeProcesses = 0
 
@@ -14,14 +14,23 @@ def getChromeProcesses():
 		if 'Chrome' in process.name:
 			nOfChromeProcesses += 1
 
+	print nOfChromeProcesses
 	return nOfChromeProcesses - chromeOffSet
+
+def getChromeProcessesProper():
+	nOfChromeProcesses = 0
+	for process in psutil.process_iter():
+		if ('Chrome Helper' in process.name) and (len(process.cmdline) > 1):
+			if ("--type=renderer" in process.cmdline) and not ("--extension-process" in process.cmdline): # Thx to Axel Rauschmayer for this criterion http://www.2ality.com/2013/09/osx-kill-chrome-tabs.html
+				nOfChromeProcesses += 1
+	return nOfChromeProcesses
 
 def saveToDatabase():
 	con = None
 	try:
 		con = sql.connect(getPathToDB())
 		cur = con.cursor()
-		cur.execute("INSERT OR REPLACE INTO chrome VALUES (?,?)", (datetime.datetime.now().strftime("%s"),getChromeProcesses()))
+		cur.execute("INSERT OR REPLACE INTO chrome VALUES (?,?)", (datetime.datetime.now().strftime("%s"),getChromeProcessesProper()))
 
 		con.commit()
 
